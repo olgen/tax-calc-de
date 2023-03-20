c6634bb816e9c011b75c6d93ae4ac2b838d65a9c
Eugen Martin <eugeniusmartinus@gmail.com>
2023-03-20 14:53:50 +0100

    add calculations of - taxable income based on different insurances - tax rate based on the taxable income - specs as source of truth

--- src/calculations.ts
+++ src/calculations.ts
@@ -1,3 +1,65 @@
-export function taxFromBrutto(brutto: number): number {
-  return brutto / 2.0;
+const grundFreibetrag = 10908;
+const firstBarrier = 15999;
+const secondBarrier = 62809;
+const thirdBarrier = 277825;
+
+const pauschBetrag = 1200;
+
+// from https://www.imacc.de/sozialabgaben-rechner-sozialversicherung/#/base-data
+function sozialAbgabenArbeitnehmer(brutto: number): number {
+  const KV = Math.min(brutto * (0.073 + 0.0065), 4987.5);
+  const PV = Math.min(brutto * (0.01525 + 0.0035), 4837.5);
+  const RV = Math.min(brutto * 0.093, 7300);
+  const AV = Math.min(brutto * 0.012, 7300);
+  return KV + PV + RV + AV;
 }
+
+export function taxEstimationFromBrutto(brutto: number): number {
+  // this is probably very wrong
+  const zVE = brutto - sozialAbgabenArbeitnehmer(brutto) - pauschBetrag;
+
+  // calc based on:
+  // https://www.finanz-tools.de/einkommensteuer/berechnung-formeln/2023
+  if (zVE <= grundFreibetrag) {
+    return 0;
+  } else if (zVE <= firstBarrier) {
+    const y = (zVE - grundFreibetrag) / 10000;
+    return (979.18 * y + 1400) * y;
+  } else if (zVE <= secondBarrier) {
+    const y = (zVE - firstBarrier) / 10000;
+    return (192.59 * y + 2397) * y + 966.53;
+  } else if (zVE <= thirdBarrier) {
+    return 0.42 * zVE - 9972.98;
+  } else {
+    return 0.45 * zVE - 18307.73;
+  }
+
+  return zVE;
+}
+
+/*
+function lohnsteuer_brutto_to_netto(brutto_monat) {
+
+		let brutto = brutto_monat * 12;
+		let lohnsteuer;
+		let netto;
+
+		if(brutto <= 10908)
+			netto = brutto;
+		else
+			if(brutto <= 15999)
+				netto = brutto - Math.round((979.18*(brutto-10909)/10000+1400)*(brutto-10909)/10000);
+			else
+				if(brutto <= 62809)
+					netto = brutto - Math.round((192.59*(brutto-15999)/10000+2397)*(brutto-15999)/10000+966.53);
+				else
+					if(brutto <= 277825)
+						netto = brutto - Math.round(brutto*0.42-9972.98);
+					else
+						netto = brutto - Math.round(brutto*0.45-18307.73);
+
+		netto = netto/12;
+
+		return netto;
+	}
+*/
--- test/calculations.spec.ts
+++ test/calculations.spec.ts
@@ -1,9 +1,42 @@
 import { assert } from "chai";
 import { describe } from "mocha";
-import { taxFromBrutto } from "../src/calculations";
+import { taxEstimationFromBrutto } from "../src/calculations";
 
 describe("Tax tests", () => {
-  it("should return 0 tax for brutto < 10800", () => {
-    assert.equal(taxFromBrutto(10000), 0);
+  /*
+    from https://www.brutto-netto-rechner.info/
+    using settings:
+    - yearly brutto
+    - year = 2023
+    - steuerfreibetrag = 0
+    - steuerkl = 1
+    - Bundesland = Berlin
+    - Alter = 25
+    - Kinder = nein
+    - Kinderfreibetrag = 0
+    - Gesetzlich versichert
+    - KV= gesetzl
+    - KV Zusatzbeitrag = 1.6
+    - RV= gesetzl
+    - AV = gesetzl
+
+    */
+  const bruttosAndTaxes = [
+    [10000, 0],
+    [15999, 66.96],
+    [20000, 729.96],
+    [30000, 2757.96],
+    [70000, 13818.0],
+    [280000, 101253.0],
+  ];
+
+  bruttosAndTaxes.forEach((rate) => {
+    it(`should return ${rate[1]} tax for brutto of ${rate[0]} `, () => {
+      const estimation = taxEstimationFromBrutto(rate[0]);
+      const deltaInPercent = (rate[1] - estimation) / 100.0;
+      console.log(deltaInPercent);
+      //   assert.isAtMost(deltaInPercent, 1);
+      assert.equal(estimation, rate[1]);
+    });
   });
 });
